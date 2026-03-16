131d482adf64d8f6e8d95c1c9f40bc5cb8c20d56
arielhalifa <arielhalifa@gmail.com>
2026-03-16 17:37:09 +0200

    modifing main file

--- first_step/kafka/kafka_consumer.py
+++ first_step/kafka/kafka_consumer.py
@@ -28,8 +28,8 @@ class Consumer:
                     continue
 
                 value = msg.value().decode("utf-8")
-                order = json.loads(value)
-                yield order
+                target = json.loads(value)
+                yield target
         except KeyboardInterrupt:
             print("\n🔴 Stopping consumer")
         finally:
@@ -40,8 +40,42 @@ class Consumer:
     @staticmethod
     def listen_attack():
         consumer.subscribe(["Attack"])
+        try:
+            while True:
+                msg = consumer.poll(1.0)
+                if msg is None:
+                    continue
+                if msg.error():
+                    print("❌ Error:", msg.error())
+                    continue
+
+                value = msg.value().decode("utf-8")
+                target = json.loads(value)
+                yield target
+        except KeyboardInterrupt:
+            print("\n🔴 Stopping consumer")
+        finally:
+            consumer.close()
+
+
 
     @staticmethod
     def listen_damage():
         consumer.subscribe(["Damage"])
+        try:
+            while True:
+                msg = consumer.poll(1.0)
+                if msg is None:
+                    continue
+                if msg.error():
+                    print("❌ Error:", msg.error())
+                    continue
+
+                value = msg.value().decode("utf-8")
+                target = json.loads(value)
+                yield target
+        except KeyboardInterrupt:
+            print("\n🔴 Stopping consumer")
+        finally:
+            consumer.close()
         
